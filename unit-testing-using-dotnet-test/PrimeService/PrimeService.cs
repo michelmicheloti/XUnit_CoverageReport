408974988a378a52339a9f04227bb2e687d4a684
Michel Micheloti <michelmicheloti@hotmail.com>
2022-03-31 16:16:35 -0300

    Init

---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeServices
+{
+    public class PrimeService
+    {
+        public string ContrataFuncionario(int idade)
+        {
+            if (idade < 16)
+            {
+                return "Não contratamos";
+            }
+
+            if (idade <= 18)
+            {
+                return "Contrato de tempo parcial";
+            }
+
+            if (idade <= 55)
+            {
+                return "Contrato de tempo integral";
+            }
+
+            return "Não contratamos";
+        }
+    }
+}
